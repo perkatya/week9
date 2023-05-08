39679049ec2a4a7a36b076fb504d6e47d31ed4f9
Ekaterina Pershina <123770548+perkatya@users.noreply.github.com>
2023-05-08 23:18:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+// задание №1
+let salaryJanitor = 40000;
+let salaryTeacher = 22000;
+console.log (`Зарплата уборщика, умеющего петь и с педагогичским образованием: ${salaryJanitor + salaryTeacher} рублей`);
+
+// задание №2
+console.log('Владивосток ' + '2000');
+
+// задание №3
+let messengerTime = 128;
+let photoTime = 254;
+let vkTime = 137;
+let ytTime = 201;
+console.log (`Время прокрастинации: ${(messengerTime + photoTime + vkTime +ytTime)/60} часов`);
+
+// задание №4
+console.log(2 ** 5);
+
+// задание №5
+console.log('Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!');
+
+// задание №6
+const greetings = "Привет";
+const name = "Кот";
+console.log(`${greetings}, ${name}!`)
+
+// задание №7
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time = 15;
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time);
+time = 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+// задание №8
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`);
